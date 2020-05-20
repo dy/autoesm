299759c4d7991a4527c82bceeb281391031ba413
dy <df.creative@gmail.com>
2020-05-20 17:24:31 -0400

    Fallback to commonjs

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ export function getFormat(url, context, defaultGetFormat) {
   if (/^file:/.test(url)) url = url.slice(8)
   let file = fs.readFileSync(url, {encoding: 'utf8'})
   if (isModule(file)) return {format: 'module'}
-  return defaultGetFormat(url, context, defaultGetFormat)
+  let dflt = defaultGetFormat(url, context, defaultGetFormat)
+  if (!dflt.format) dflt.format = 'commonjs'
+  return dflt
 }
