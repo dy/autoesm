34112eaf22388d4271aec1ca29cfe8cb64fcc57f
dy <df.creative@gmail.com>
2020-05-20 22:19:15 -0400

    url can be path

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import fs from 'fs'
 import { fileURLToPath } from 'url'
 
 export function getFormat(url, context, defaultGetFormat) {
-  let path = fileURLToPath(url)
-  let file = fs.readFileSync(path, {encoding: 'utf8'})
+  if (/^file:/.test(url)) url = fileURLToPath(url)
+  let file = fs.readFileSync(url, {encoding: 'utf8'})
   if (isModule(file)) return {format: 'module'}
-  let dflt = defaultGetFormat(path, context, defaultGetFormat)
+  let dflt = defaultGetFormat(url, context, defaultGetFormat)
   if (!dflt.format) dflt.format = 'commonjs'
   return dflt
 }
