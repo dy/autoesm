0f56491606fcacb049a9dc4ec2b223a5f742839e
dy <df.creative@gmail.com>
2020-05-20 22:13:54 -0400

    Fix paths

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import isModule from 'is-module'
 import fs from 'fs'
+import { fileURLToPath } from 'url'
 
 export function getFormat(url, context, defaultGetFormat) {
-  if (/^file:/.test(url)) url = url.slice(8)
-  let file = fs.readFileSync(url, {encoding: 'utf8'})
+  let path = fileURLToPath(url)
+  let file = fs.readFileSync(path, {encoding: 'utf8'})
   if (isModule(file)) return {format: 'module'}
-  let dflt = defaultGetFormat(url, context, defaultGetFormat)
+  let dflt = defaultGetFormat(path, context, defaultGetFormat)
   if (!dflt.format) dflt.format = 'commonjs'
   return dflt
 }
